0dbee2b47b6c4eead90e818e3de1ff4a08e89ace
Reemaalt <reema.altj@gmail.com>
2023-12-01 00:47:33 +0300

    did the main

--- cProject.C
+++ cProject.C
@@ -10,5 +10,40 @@ struct Taxi {
     float rate;
     float minCharge;
     char state; // 'A' for available, 'R' for in a ride
-    struct Taxi* next; // Self-referential structure pointer
+    struct Taxi* next; // Self-referential 
 };
+
+//void addTripCar() 1
+
+//void setTripCar (char* category, float* rate) 2
+
+//void writeCarsInRide (char* fileName) 3
+
+//printList() 4
+
+int main() {
+    // Declare a pointer to a linked list of struct Taxi
+    struct Taxi* list = NULL;  // an empty list
+
+    // Assign the first node to the list سويته عشان ما ننسى بس مافيه نود اصلا
+    list = &taxi1; 
+
+    // Call the function addTripCar to read the content  of the given text file: “Taxies.txt”, 
+    addTripCar(&list, "Taxies.txt");
+
+    // Call the function setTripCar with the given information
+    setTripCar(list, "Business", 4.5);
+    setTripCar(list, "Family", 5.0);
+    setTripCar(list, "Family", 4.0);
+    setTripCar(list, "standard", 3.4);
+    setTripCar(list, "standard", 5.0);
+
+    // Call the function writeCarsInRide to update the txt file with cars in trip.
+    writeCarsInRide(&list, "Taxies.txt");
+
+    // Call the function printList to print the cars information
+    printList(list);
+
+    
+    return 0;
+}
